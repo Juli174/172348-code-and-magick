0896e1a98ed33da5c081c68141dff2aa452ad79c
julia <glad198787@mail.ru>
2016-06-11 01:08:13 +0300

    module3-task2

--- src/form.js
+++ src/form.js
@@ -6,8 +6,12 @@
   var formCloseButton = document.querySelector('.review-form-close');
 
   var formFieldName = document.querySelector('#review-name');
+  var formReviewText = document.querySelector('#review-text');
   var linkFieldName = document.querySelector('.review-fields-name');
   var reviewFieldName = document.querySelector('.review-fields-text');
+
+  var reviewFields = document.querySelector('.review-fields');
+  var submitBtn = document.querySelector('.review-submit');
 
   if(reviewFieldName){
     reviewFieldName.style.display = 'none';
@@ -17,11 +21,11 @@
     var marks = document.getElementsByName('review-mark');
     if(!marks) return;
     var mark;
-    marks.forEach(function(item, i){
-      if(item.checked){
-        mark = item.value;
+    for(var i = 0; i < marks.length; i++){
+      if(marks[i].checked){
+        mark = marks[i].value;
       }
-    });
+    }
     return mark;
   }
 
@@ -29,15 +33,19 @@
     var currentMark = getMark();
     if(currentMark < 3){
       if(reviewFieldName){
-        reviewFieldName.style.display = 'block';
-      }
+        reviewFieldsFn();
+        reviewFieldName.style.display = 'inline';
+      } 
+    } else{
+      reviewFieldsFn();
+      reviewFieldName.style.display = 'none';
     }
   }
 
   displayReviewLink();
 
-  function setMark(selectedMark){
-      displayReviewLink();
+  window.setMark = function(chosen){
+    displayReviewLink();
   }
 
   formOpenButton.onclick = function(evt) {
@@ -50,10 +58,33 @@
     formContainer.classList.add('invisible');
   };
 
+  function reviewFieldsFn(){
+    var mark = getMark();
+    if(!formFieldName.value.length || (!formReviewText.value && mark < 3)){
+      submitBtn.setAttribute('disabled', true);
+      reviewFields.style.display = 'inline-block';
+    } else{
+      submitBtn.removeAttribute('disabled');
+      reviewFields.style.display = 'none';
+    }
+  }
+
   formFieldName.onkeyup = function(evt){
     if(formFieldName.value.length > 0){
         linkFieldName.style.display = 'none';
+    } else{
+      linkFieldName.style.display = 'inline';
     }
+    reviewFieldsFn();
+  };
+
+  formReviewText.onkeyup = function(){
+    if(formReviewText.value.length > 0){
+      reviewFieldName.style.display = 'none';
+    } else{
+      reviewFieldName.style.display = 'inline';
+    }
+    reviewFieldsFn();
   };
 
 })();
