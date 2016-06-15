4596b114f2dcf7e187a935ddac8f5ab3a81aa1b2
yv.gladysheva <yv.gladysheva@prof-itgroup.ru>
2016-06-15 16:04:14 +0300

    module5-task1

--- src/reviews.js
+++ src/reviews.js
@@ -1,16 +1,23 @@
 var REVIEWS_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
 
 var Filter = {
-	'ALL': 'all',
-	'RECENT': 'recent',
-	'GOOD': 'good',
-	'BAD': 'bad',
-	'POPULAR': 'popular'
+	'ALL': 'reviews-all',
+	'RECENT': 'reviews-recent',
+	'GOOD': 'reviews-good',
+	'BAD': 'reviews-bad',
+	'POPULAR': 'reviews-popular'
 };
+
+var DEFAULT_FILTER = Filter.ALL;
 
 var reviewsFilter = document.querySelector('.reviews-filter');
 if(reviewsFilter){
 	reviewsFilter.classList.add('invisible');
+}
+
+var reviewsSection = document.querySelector('.reviews');
+if(reviewsSection){
+	reviewsSection.classList.add('reviews-list-loading');
 }
 
 var reviewsContainer = document.querySelector('.reviews-list');
@@ -23,8 +30,6 @@ if('content' in templateElement){
 } else{
 	var templateClone = templateElement.querySelector('.review');
 }
-
-
 
 function getReviewElement(data, container){
 	var element = templateClone.cloneNode(true);
@@ -60,26 +65,33 @@ function getFilteredReviews(reviews, filter){
 
 	switch(filter){
 		case Filter.RECENT:
+			reviewsToFilter = reviewsToFilter.filter(function(item, i){
+				var days = (new Date() - new Date(item.date)) / 1000 / 60 / 60 / 24;
+				if((days >= 0) && (days < 5)){
+					return true;
+				}
+			});
 			reviewsToFilter.sort(function(a, b){
-
+				return new Date(b.date) - new Date(a.date);
 			});
 			break;
-		case 'GOOD':
-			reviewsToFilter.filter(function(review){
+		case Filter.GOOD:
+			reviewsToFilter = reviewsToFilter.filter(function(review){
 				return review.rating >= 3;
 			});
 			break;
-		case 'BAD':
-			reviewsToFilter.filter(function(review){
+		case Filter.BAD:
+			reviewsToFilter = reviewsToFilter.filter(function(review){
 				return review.rating < 3;
 			});
 			break;
-		case 'POPULAR':
+		case Filter.POPULAR:
 			reviewsToFilter.sort(function(a, b){
-				return a.review_usefulness - b.review_usefulness;
+				return b.review_usefulness - a.review_usefulness;
 			});
 			break;
 		default:
+			reviewsToFilter = reviews;
 			break;
 	}
 	return reviewsToFilter;
@@ -104,9 +116,14 @@ function getReviews(callback){
 
 	xhr.onload = function(evt){
 		var loadedData = JSON.parse(evt.target.response);
-		console.log(loadedData);
+		reviewsSection.classList.remove('reviews-list-loading');
 		callback(loadedData);
 	};
+
+	xhr.onerror = function(){
+		reviewsSection.classList.remove('reviews-list-loading');
+		reviewsSection.classList.add('reviews-load-failure');
+	}
 
 	xhr.open('GET', REVIEWS_LOAD_URL);
 	xhr.send();
@@ -115,6 +132,7 @@ function getReviews(callback){
 getReviews(function(loadedReviews){
 	reviews = loadedReviews;
 	setFiltersEnabled(true);
+	setFilterEnabled(DEFAULT_FILTER);
 });
 
 if(reviewsFilter){
