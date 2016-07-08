1678c078aee441a15a32c37a3c196254d56744fb
yv.gladysheva <yv.gladysheva@prof-itgroup.ru>
2016-07-08 19:07:06 +0300

    module8-task2

--- src/methods/renderReviews.js
+++ src/methods/renderReviews.js
@@ -1,22 +1,76 @@
 var constant = require('../values/constants');
 var pageElement = require('../values/elements');
-var reviewElement = require('./reviewElement');
+var getReviewElement = require('./reviewElement');
+
+var Review = function(data, container) {
+  var self = this;
+  self.data = data;
+  self.element = getReviewElement(data, container);
+
+  self.isPressedSelected = function(element) {
+    if(element.classList.contains('review-quiz-answer-active')){
+      return true;
+    }
+    return false;
+  };
+
+  self.getElementNumber = function(element){
+    var ind;
+    for (var i = 0; i < self.quizs[i].length; i++) {
+      if (self.quizs[i] == element) {
+        ind = i;
+      }
+    }
+    return ind;
+  };
+
+  self.onClick = function(evt) {
+    // for (var i = 0; i < self.quizs.length; i++) {
+    //   if(self.quizs[i].classList.contains('review-quiz-answer-active') && self.quizs[i] == evt.target){
+    //     evt.target.classList.remove('review-quiz-answer-active');
+    //   } else {
+    //     self.quizs[i].classList.remove('review-quiz-answer-active');
+    //     evt.target.classList.add('review-quiz-answer-active');
+    //   }
+    // }
+    var ind = self.getElementNumber(evt.target);
+    for (var i = 0; i < self.quizs.length; i++) {
+      if (self.isPressedSelected(self.quizs[i])) {
+        self.quizs[i].classList.remove('review-quiz-answer-active');
+      }
+    }
+
+  };
+
+  self.quizs = self.element.getElementsByClassName('review-quiz-answer');
+  for (let i = 0; i < self.quizs.length; i++){
+    self.quizs[i].addEventListener('click', self.onClick);
+  }
+
+  self.remove = function(){
+    for(let i = 0; i < self.quizs.length; i++){
+      self.quizs[i].removeEventListener('click', self.onClick);
+    }
+  };
+};
 
 function renderReviews(page, replace, filteredReviews) {
-    if(replace) {
-      pageElement.reviewsContainer.innerHTML = '';
-    }
-
-    var from = page * constant.PAGE_SIZE;
-    var to = from + constant.PAGE_SIZE;
-
-    if (to >= filteredReviews.length) {
-      pageElement.moreReviewsBtn.classList.add('invisible');
-    }
-
-    filteredReviews.slice(from, to).forEach(function(item) {
-      reviewElement.getReviewElement(item, pageElement.reviewsContainer);
-    });
+  if(replace) {
+    pageElement.reviewsContainer.innerHTML = '';
   }
+
+  var from = page * constant.PAGE_SIZE;
+  var to = from + constant.PAGE_SIZE;
+
+  if (to >= filteredReviews.length) {
+    pageElement.moreReviewsBtn.classList.add('invisible');
+  }
+
+  filteredReviews.slice(from, to).forEach(function(item) {
+    var element = new Review(item, pageElement.reviewsContainer);
+  });
+
+
+}
 
 module.exports = renderReviews;
--- src/methods/reviewElement.js
+++ src/methods/reviewElement.js
@@ -4,9 +4,19 @@ var getTemplateClone = require('./template');
 
 var templateClone = getTemplateClone(pageElement.templateElement, 'review');
 
-module.exports = {
-  getReviewElement: function(data, container){
-    var element = templateClone.cloneNode(true);
+// var Review = function(data, container){
+//   this.data = data;
+//   this.element = getReviewElement(data, container);
+//   this.quizs = this.element.getElementsByClassName('.review-quiz-answer');
+//   for (let i = 0; i < this.quizs.length; i++){
+//     this.quizs[i].onQuizClick = function(evt){
+//       console.log('works!');
+//     };
+//   }
+// };
+
+function getReviewElement(data, container){
+  var element = templateClone.cloneNode(true);
     element.querySelector('.review-text').textContent = data.description;
     element.querySelector('.review-rating').textContent = data.rating;
 
@@ -31,5 +41,36 @@ module.exports = {
 
     container.appendChild(element);
     return element;
-  }
 }
+
+module.exports = getReviewElement;
+
+// module.exports = {
+//   getReviewElement: function(data, container){
+//     var element = templateClone.cloneNode(true);
+//     element.querySelector('.review-text').textContent = data.description;
+//     element.querySelector('.review-rating').textContent = data.rating;
+
+//     var userPic = new Image();
+//     var userPicLoadTimeout;
+
+//     userPic.onload = function(evt) {
+//       clearTimeout(userPicLoadTimeout);
+//       element.querySelector('.review-author').src = evt.target.src;
+//     };
+
+//     userPic.onerror = function() {
+//       element.classList.add('review-load-failure');
+//     };
+
+//     userPic.src = data.author.picture;
+
+//     userPicLoadTimeout = setTimeout(function() {
+//       userPic.src = '';
+//       element.classList.add('review-load-failure');
+//     }, constant.LOAD_TIMEOUT);
+
+//     container.appendChild(element);
+//     return element;
+//   }
+// }
